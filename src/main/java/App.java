6e90a2d817d3e478fa79f9f9bbccda0f71dc87e9
pankushm88 <pankushm88@gmail.com>
2020-09-27 17:07:01 +0530

    Jenkins Example committed

---
+++ src/main/java/App.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author panmahaj
+ *
+ */
+public class App {
+
+	    public String play(int number) {
+
+	        if (number == 0) throw new IllegalArgumentException("Number must not be 0");
+	        if (number % 3 == 0) return "Fizz";
+	        if (number % 5 == 0) return "Buzz";
+
+
+	        return String.valueOf(number);
+	    }
+}
